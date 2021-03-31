37567fd7af36a7bd09b8b0eb8337f687e0d36b66
Thiago Souto <soutosss@gmail.com>
2021-03-31 19:36:49 -0300

    setup jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,36 @@
 plugins {
     java
-    `java-gradle-plugin`
     `kotlin-dsl`
     kotlin("jvm") version "1.4.31"
     id("java-gradle-plugin")
     id("maven-publish")
+    jacoco
     id("com.gradle.plugin-publish") version "0.12.0"
     id("io.gitlab.arturbosch.detekt") version "1.16.0"
 }
 
-group ="dev.thiagosouto"
-version ="0.6.0"
+group = "dev.thiagosouto"
+version = "0.6.0"
 description = "A plugin to generate BOM from gradle projects"
+
+jacoco {
+    toolVersion = "0.8.6"
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.isEnabled = true
+        csv.isEnabled = false
+    }
+}
 
 gradlePlugin {
     plugins {
         create("bom-plugin") {
             id = "dev.thiagosouto.plugins.bom-plugin"
             implementationClass = "dev.thiagosouto.plugins.bom.BomPlugin"
-            displayName= "bom-plugin"
+            displayName = "bom-plugin"
             description = "A plugin to generate BOM from gradle projects"
         }
     }
@@ -35,7 +47,6 @@ dependencies {
     testImplementation("com.google.truth:truth:1.1.2")
     testImplementation("junit:junit:4.13.2")
     testImplementation("dev.thiagosouto:file-butler:0.3.0")
-
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.16.0")
 }
 
